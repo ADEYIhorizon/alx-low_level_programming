316a21f3f49e899cd5d3a9137fd84241b88417eb
Adeyi Qudus Babatunde <adeyiqudus001@gmail.com>
2022-03-25 03:23:43 +0100

    String Concatenation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,6 +7,7 @@
  * @n: int parameter
  * Return: *dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
     int i;
@@ -24,7 +25,9 @@ char *_strncat(char *dest, char *src, int n)
     }
 
     for (i = 0 ; i < n && src[i] != '\0' ; i++)
-        dest[m + i] = src[i];
+        {
+            dest[m + i] = src[i];
+        }
     dest[m + i] = '\0';
 
     return (dest);
