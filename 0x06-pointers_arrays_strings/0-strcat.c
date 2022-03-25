8841c8dd2b1f6fd3ffdd969045b8e8c13aa93721
Adeyi Qudus Babatunde <adeyiqudus001@gmail.com>
2022-03-25 03:58:07 +0100

    String Concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,34 +1,32 @@
 #include "main.h"
 
 /**
- * _strcat - Function Concatenate of a string
- *
- * @dest: destination string; src string is appended to it
- *
- * @src: source string; gets appended to dest string
- *
- * Return: pointer to dest
+ * *_strcat - function commute srtings
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return value of dest
  */
 
 char *_strcat(char *dest, char *src)
 {
 	int i;
-    	int j;
+	int j;
 
 	i = 0;
 	j = 0;
 
 	while (dest[i] != '\0')
-    	{
-        	i++;
+	{
+		i++;
 	}
 
 	while (src[j] != '\0')
 	{
-        	dest[1] = src[j];
-        	j++;
-        	i++;
-    	}
-    	dest[i] = '\0';
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
 	return (dest);
 }
