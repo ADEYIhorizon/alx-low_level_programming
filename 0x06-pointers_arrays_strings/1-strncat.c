7018b902c9437d0d92664d915eeaeeefc99dd704
Adeyi Qudus Babatunde <adeyiqudus001@gmail.com>
2022-03-25 03:22:29 +0100

    String Concatenation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,28 +4,28 @@
  * _strcat - Concatenate of a string
  * @dest: param pointer to a char
  * @src: param pointer to a char
- * @n: size of byte
- * Return: return value of dest
+ * @n: int parameter
+ * Return: *dest
  */
 char *_strncat(char *dest, char *src, int n)
+{
+    int i;
+    int m;
+
+    m = 0;
+    
+    for(i = 0; i < 1000; i++)
     {
-        int i;
-        int m;
-
-        m = 0;
-        
-        for(i = 0; i < 1000; i++)
+        if (dest[i] == '\0')
         {
-            if (dest[i] == '\0')
-            {
-                break;
-            }
-            m++;
+            break;
         }
-
-        for (i = 0 ; i < n && src[i] != '\0' ; i++)
-            dest[m + i] = src[i];
-        dest[m + i] = '\0';
-
-        return (dest);
+        m++;
     }
+
+    for (i = 0 ; i < n && src[i] != '\0' ; i++)
+        dest[m + i] = src[i];
+    dest[m + i] = '\0';
+
+    return (dest);
+}
