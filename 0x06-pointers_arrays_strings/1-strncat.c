0cd081992620047d38b96449c031e1fa55e70748
Adeyi Qudus Babatunde <adeyiqudus001@gmail.com>
2022-03-25 04:00:19 +0100

    Conccatenation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,33 +1,33 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenate of a string
- * @dest: param pointer to a char
- * @src: param pointer to a char
- * @n: int parameter
+ * _strncat - two words
+ * @dest : pointer to char param
+ * @src : pointer to char param
+ * @n : int parameter
  * Return: *dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-    int i;
-    int m;
+	int m;
+	int i;
 
-    m = 0;
-    
-    for(i = 0; i < 1000; i++)
-    {
-        if (dest[i] == '\0')
-        {
-            break;
-        }
-        m++;
-    }
+	m = 0;
 
-    for (i = 0; i < n && src[i] != '\0'; i++)
-        {
-            dest[m + i] = src[i];
-        }
-    dest[m + i] = '\0';
-    return (dest);
+	for (i = 0; i < 1000; i++)
+	{
+		if (dest[i] == '\0')
+		{
+			break;
+		}
+		m++;
+	}
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[m + i] = src[i];
+	}
+	dest[m + i] = '\0';
+	return (dest);
 }
