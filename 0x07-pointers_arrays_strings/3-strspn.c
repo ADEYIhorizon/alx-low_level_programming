48800fef3b96e8d86114485fd2ecbfba7c4db021
Adeyi Qudus Babatunde <adeyiqudus001@gmail.com>
2022-03-29 03:16:14 +0100

    string span

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,22 +6,20 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int n = 0;
-	char *m = accept;
+	unsigned int c = 0;
+	char *t = accept;
 
-	while(*s++)
+	while (*s++)
 	{
 		while (*accept++)
-		{
-			if (*(s-1) == *(accept -1))
+			if (*(s - 1) == *(accept - 1))
 			{
-				n++;
+				c++;
 				break;
 			}
-		}
 		if (!(*--accept))
 			break;
-		accept = m;
+		accept = t;
 	}
-	return (n);
+	return (c);
 }
