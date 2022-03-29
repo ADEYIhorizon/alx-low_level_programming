0b34a6b113ce6c5c6c02af2d23f68fe91e45c9c5
Adeyi Qudus Babatunde <adeyiqudus001@gmail.com>
2022-03-29 02:03:29 +0100

    _memset function, More pointer and array

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * _memset - fill the first n bytes of the memory area pointed to by s with the constant byte b
+ * @s: address of memory to print
+ * @n: the size of the memory to print
+ * @b: the constant byte
+ *
+ * Return: *s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+	
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Repository of Pointer to poniter
