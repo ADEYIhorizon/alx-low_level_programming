d08789e8d7e666cad730d47eb20b59bb23ef2b83
Adeyi Qudus Babatunde <adeyiqudus001@gmail.com>
2022-03-29 02:17:19 +0100

    _memset function, More pointer and array

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * _memset - fill the first n bytes of the memory area
- * @s: address of memory to print
- * @n: the size of the memory to print
- * @b: the constant byte
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
  * Return: *s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
